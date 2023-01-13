40e76b9252d8d7d5ad6368cf441ce13ecdb44691
Chetyrkin Alex <pyroponya@gmail.com>
2023-01-13 16:55:02 +0300

    test

--- pages/edit.vue
+++ pages/edit.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="edit_container">
     <p>edit page</p>
+    <input v-model="someData" type="text">
+    <br/>
+    <button @click="testSave">SAVE</button>
+    <br/>
+    <button @click="testLoad">LOAD</button>
   </div>
 </template>
 
 <script setup>
-
+const someData = ref('');
+const testSave = async () => {
+  await useStorage().setItem('test:foo', { testData: someData.value })
+}
+const testLoad = async () => {
+  someData.value = await useStorage().getItem('test:foo')
+}
 </script>
 
 <style scoped>
